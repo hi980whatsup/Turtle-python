299993fe90839bd5928d4508592c22a09c568afa
hi980whatsup <142428966+hi980whatsup@users.noreply.github.com>
2025-04-05 21:24:17 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("light blue")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to Turtle network")
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i=i+1
+turtle.done()
